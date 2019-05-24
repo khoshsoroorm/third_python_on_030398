02bb8124bd015b95157cc3f23d1666c8e6777a33
khoshsoroorm <khoshsoroorm@gmail.com>
2019-05-24 11:50:20 +0330

    zeta

--- reiman_zeta.py
+++ reiman_zeta.py
@@ -1,11 +1,14 @@
 import math
+import time
 
 num_1 = 1000
 out = 0
 i = 1
+time_1 = time.time()
 while i < num_1:
     out = out + (1 / i ** 2)
     i += 1
-
+time_2 = time.time()
 print(math.sqrt(out * 6), '\n')
 print(math.pi)
+print('the time :', time_2 - time_1)
