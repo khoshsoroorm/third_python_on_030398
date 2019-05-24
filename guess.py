106f923ab5048927ea6c50c9b834a249afe55bc3
khoshsoroorm <khoshsoroorm@gmail.com>
2019-05-24 09:17:05 +0330

    import random library

--- guess.py
+++ guess.py
@@ -1,11 +1,16 @@
-secret = 7
+import random
+secret = random.randint(1,10)
 print('guess if you are a genius')
+
 while True:
+
     guess = int(input('guess the secret number:'))
-    if guess == secret:
+    if guess < secret:
+        print('javab kochiktar ast \n try again')
+    elif guess > secret:
+        print(' javab bozogtar az male mas')
+    elif guess == secret:
         print('very good \n you are a genius')
         break
-    elif guess < secret:
-        print('javab kochiktar ast \n try again')
-    elif guess > secret :
-        print(' javab bozogtar az male mas')
+
+
