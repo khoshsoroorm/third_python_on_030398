b68d504ad8c76ba4b6f915f86e404daceda2c93f
khoshsoroorm <khoshsoroorm@gmail.com>
2019-05-24 09:52:27 +0330

    is prime

--- prime_num.py
+++ prime_num.py
@@ -1,1 +1,14 @@
 # prime num with i**2 < n
+
+num_1 = 13
+
+i = 2
+is_prime = True
+
+while i ** 2 < num_1:
+    if num_1 % i == 0:
+        is_prime = False
+        break
+    i = i + 1
+
+print(is_prime)
